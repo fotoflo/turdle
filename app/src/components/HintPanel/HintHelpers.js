175d2ae39e81c0b059cf22ead3ea9bd53dbfef38
Alex Miller <fotoflo@gmail.com>
2022-01-19 08:32:28 +0800

    supporting functions all passing

--- app/src/components/HintPanel/HintHelpers.js
+++ app/src/components/HintPanel/HintHelpers.js
@@ -1,27 +1,63 @@
+import { array } from 'prop-types'
 import wordList from '../../FiveLetterWords.json'
 
 export const getCharsFromKeyboardState = (keyboardState, state) => {
   return Object.entries(keyboardState)
-  .filter( letterCount => letterCount[1] === state ) // [t, 2] get all entries with 2
-  .map( letterCount => letterCount[0] )  //  [t, ...] array return t
-  .join('') // join 
+    .filter( letterCount => letterCount[1] === state ) // [t, 2] get all entries with 2
+    .map( letterCount => letterCount[0] )  //  [t, ...] array return t
+    .join('') // join 
 } 
 
-export const findWordsWithChars  = (wordList, chars) => {
+export const getKeyValuePairsByValueFromGameState = (gameState, value) => {
+
+  return gameState
+    .filter( pair =>{
+      const [key, value] = getKeyValueFromPair(pair)
+      return value == 3
+    } )
+} 
+
+Array.prototype.findWordsWithChars = function(chars){
   const charsRegex = new RegExp(`[${chars}]`)
-  return wordList.filter(  word => word.match(charsRegex)  )
+  return this.filter(  word => word.match(charsRegex)  )
 }
 
-export const findWordsWithoutChars = (wordList, chars) => {
+Array.prototype.findWordsWithoutChars = function (chars){
   const charsRegex = new RegExp(`[${chars}]`)
-  return wordList.filter(  word => !word.match(charsRegex) )
+  return this.filter(  word => !word.match(charsRegex) )
 }
 
-export const findWordsWithCharInSlot = (wordList, CharSlotPairs) => {
-  return CharSlotPairs  // [{x:0}, {r:4}]
-    .flatmap( pair => {
-      const char = Object.keys(pair)[0]
-      const slot = pair[char]
-      return wordList.filter(  word => word[slot] === char ) 
-    })
+Array.prototype.findWordsWithCharInSlot = function (charSlotPair){
+  //  charSlotPairs: {char: r, slot: 4}
+  return this  // wordlist = ['xenon', 'rewax', 'roger', 'bob']
+    .filter(  word => word[charSlotPair.slot] === charSlotPair.char ) 
+}
+
+Array.prototype.findWordsWithoutCharInSlot = function (charSlotPair){
+  //  charSlotPairs: {char: r, slot: 4}
+  return this  // wordlist = ['xenon', 'rewax', 'roger', 'bob']
+    .filter(  word => word[charSlotPair.slot] !== charSlotPair.char ) 
+}
+
+
+export const getKeyValueFromPair = (pair) => {
+  const char = Object.keys(pair)[0]
+  const slot = pair[char]
+  return [char, slot]
+}
+
+Array.prototype.indexOfObject = function(obj){
+  for(let i = 0; i < this.length; i++){
+    if(JSON.stringify(this[i]) === JSON.stringify(obj) ){
+      return i
+    }
+  }
+}
+
+export const filterWordList = (wordList, gameState, KeyboardState) => {
+  const exactMatches = getKeyValuePairsFromGameState(gameState,3)
+  const loc = wordList.indexOfObject(exactMatches)
+  debugger
+  wordList.findWordsWithCharInSlot()
+  
 }
--- app/src/components/HintPanel/HintPanel.test.js
+++ app/src/components/HintPanel/HintPanel.test.js
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/react';
 // import HintPanel from './HintPanel';
-import {getCharsFromKeyboardState,
-  findWordsWithChars,
-  findWordsWithoutChars,
-  findWordsWithCharInSlot
+import the_wordlist from '../../FiveLetterWords.json'
+
+import {
+  getCharsFromKeyboardState,
+  getKeyValuePairsByValueFromGameState,
+  //Array.prototype.findWordsWithChars,
+  //Array.prototype.findWordsWithoutChars,
+  //Array.prototype.findWordsWithCharInSlot,
+  //Array.prototype.findWordsWithoutCharInSlot,
+  //Array.prototype.indexOfObject(obj)
+  getKeyValueFromPair,
+  filterWordList
 } from './HintHelpers';
 
 import '@testing-library/jest-dom';
@@ -18,7 +26,7 @@ describe( 'getCharsFromKeyboardState', ()=>{
   const sampleKeyboardState = {"q":1,"w":1};
 
 	it(`it should return the letters with keyboardState 1" `, () => {
-    let result = getCharsFromKeyboardState(sampleKeyboardState, 1)
+    const result = getCharsFromKeyboardState(sampleKeyboardState, 1)
 	  expect(result == "qw")
 	}) 
 })
@@ -28,28 +36,79 @@ describe( 'findWordsWithChars', ()=>{
   const wordlist = ['xenon', 'rewax', 'roger', 'bob']
 
 	it(`it should return a list of words that have the chars" `, () => {
-    let result = findWordsWithChars(wordlist, chars)
+    const result = wordlist.findWordsWithChars(chars)
+    console.log(result)
     expect( result.sort() ).toEqual( [ 'xenon', 'rewax' ].sort() )
 	}) 
 })
 
-describe( 'findWordsWithChars', ()=>{
+describe( 'findWordsWithoutChars', ()=>{
   const chars = "x";
   const wordlist = ['xenon', 'rewax', 'roger', 'bob']
 
 	it(`it should return a list of words that dont have the chars" `, () => {
-    let result = findWordsWithoutChars(wordlist, chars)
+    let result = wordlist.findWordsWithoutChars( chars)
     expect( result.sort() ).toEqual( [ 'roger', 'bob' ].sort() )
 	}) 
 })
 
-describe.only( 'findWordsWithCharInSlot', ()=>{
-  const charSlotPairs = [{x:0}, {r:4}];
+describe( 'Array.prototype.findWordsWithCharInSlot', ()=>{
+  const charSlotPair = {char: "x", slot: 0};
+  const wordlist = ['xenon', 'rewax', 'roger', 'bob', 'xylophone']
+
+	it(`it should return a list of words have the right char in the right slot" `, () => {
+    const result = wordlist.findWordsWithCharInSlot(charSlotPair)
+    expect( result.sort() ).toEqual( [ 'xylophone', 'xenon' ].sort() )
+	}) 
+})
+
+describe( 'Array.prototype.findWordsWithoutCharInSlot', ()=>{
+const charSlotPair = {char: "x", slot: 0}
   const wordlist = ['xenon', 'rewax', 'roger', 'bob']
 
 	it(`it should return a list of words have the right char in the right slot" `, () => {
-    let result = findWordsWithCharInSlot(wordlist, charSlotPairs)
-    expect( result.sort() ).toEqual( [ 'roger', 'xenon' ].sort() )
+    const result = wordlist.findWordsWithoutCharInSlot(charSlotPair)
+    expect( result.sort() ).toEqual( [  'rewax', 'roger', 'bob' ].sort() )
+	}) 
+})
+
+describe( 'getKeyValueFromPair', ()=>{
+  const keyValuePair = {x:0}
+
+	it(`should return a key and value from a KeyValuePair" `, () => {
+    const result = getKeyValueFromPair(keyValuePair)
+    expect( result ).toEqual( [ 'x', 0 ] )
+	}) 
+})
+
+
+describe( 'getKeyValuePairsByValueFromGameState', ()=>{
+  const sampleGameState = [{w:3},{c:1},{t:1},{u:3},{}];
+
+	it(`it should return the key/value pairs with value 3" `, () => {
+    const result = getKeyValuePairsByValueFromGameState(sampleGameState, 3)
+	  expect(result.sort() ).toEqual([{w:3}, {u:3}].sort())
+	}) 
+})
+
+describe( 'Array.prototype.indexOfObject(obj)', ()=>{
+  const gameState = [{"r":2},{"o":1},{"g":1},{"u":1},{"e":3}, {"f":4}]
+  it(`should return the index of the matching object" `, () => {
+    const result = gameState.indexOfObject({e:3})
+    expect( result ).toEqual( 4 )
+	}) 
+})
+
+
+describe.skip( 'filterWordList', ()=>{
+  const gameState = [{"r":2},{"o":1},{"g":1},{"u":1},{"e":3}]
+  const keyboardState =  {"t":0,"r":2,"i":0,"o":1,"g":1,"u":1,"e":3,"c":1}
+  const wordlist = the_wordlist;
+
+	it(`it should return a list of words have the right char in the right slot" `, () => {
+    const result = filterWordList(wordlist, gameState, keyboardState)
+    console.log("RESULT:", result)
+    expect( result ).toEqual( [ 'roger', 'xenon' ] )
 	}) 
 })
 
