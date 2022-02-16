2cc2c41c51fd1720a0d320040f8537babeb3b3ea
Alex Miller <fotoflo@gmail.com>
2022-02-16 14:49:28 +0800

    writing the dictionaries as proper json

--- helpers/filterNLetters.js
+++ helpers/filterNLetters.js
@@ -6,19 +6,39 @@ const now = new Date()
 
 const conf = {
   minWordSize : 1,
-  fileNamePrefix : `./output-files/${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}-`
+  outputDir: "./outputFiles",
 }
 
-let dictHasBiggerWords = true;
+if (!fs.existsSync(conf.outputDir)){
+    fs.mkdirSync(conf.outputDir);
+}
+
+const createdFiles = []
 
 lineReader.eachLine(inputFilename, function(line, last) {
-  const filename = `${conf.fileNamePrefix}-${line.length}-letter-words.txt`
-  writeLineToFile(filename, `"${line}",`)
-  console.log(filename, " - ", line, line.length);
+  const filename = `${conf.outputDir}/${line.length}-letter-words.txt`
+
+  if( !fs.existsSync(filename) ){
+    writeLineToFile(filename, `["${line}"`)
+    createdFiles.push(filename)
+    console.log(`creating ${filename}`)
+    return
+  } 
+
+  writeLineToFile(filename, `,"${line}"`)
+  // console.log(filename, " - ", line, line.length);
+
+  if(last){
+    for(i in createdFiles){
+      console.log(`closing file ${createdFiles[i]}`);
+      writeLineToFile(createdFiles[i], "]")
+    }
+  }
+
 })
 
 const writeLineToFile = (filename, line) =>{
-  fs.appendFile(filename, line, function (err) {
+  fs.appendFile(filename, line, (err) => {
     if (err) {
       // append failed
       console.log("error writing file")
