089141e83ae0e97d716ba0bc5209ca1f560da262
Alex Miller <fotoflo@gmail.com>
2022-02-16 15:24:18 +0800

    create the wordlist api

---
+++ wordcheater/pages/api/wordlist.js
@@ -0,0 +1,21 @@
+/// route at http://localhost:3000/api/wordlist?wordlength=5
+
+const fsp = require('fs').promises
+
+export default async function handler(req, res) {
+  const wordlength = req.query.wordlength
+  
+  try {
+    if(!wordlength) throw "no wordlength"
+
+    const file_data = await fsp.readFile(`dictonaries/${wordlength}-letter-words.json`)
+    // looks from the root of the project
+
+    const json_data = JSON.parse(file_data)
+    res.status(200).json(json_data)
+  }
+  catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Error reading data, ${error}` })
+  }
+}
