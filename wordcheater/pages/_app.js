f96ad64b989a53403d2c13420d94ae90aaa2f404
Alex Miller <fotoflo@gmail.com>
2022-02-17 13:45:18 +0800

    refactor underway

--- wordcheater/pages/_app.js
+++ wordcheater/pages/_app.js
@@ -1,121 +1,68 @@
-import React, { useEffect, useState } from "react";
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styled, { ThemeProvider } from "styled-components";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import {Navbar, Container} from 'react-bootstrap'
-import { lightTheme, darkTheme, GlobalStyles } from "../components/Themes";
-import { FaLightbulb, FaRegQuestionCircle } from 'react-icons/fa';
+export default function Home({ Component, wordList }) {
+  useEffect(() => {
+    console.log(wordList);
+  }, []);
 
-
-
-import ThemeToggleSwitch from '../components/ThemeToggleSwitch';
-import GameBoard from '../components/GameBoard';
-import HelpModal from '../components/HelpModal';
-
-
-function App({wordList}) {
-  console.log("wordList:", wordList)
-  
-  const [word, setWord] = useState("hello");
-  const [theme, setTheme] = useState("light");
-  const [showModal, setShowModal] = useState(false);
-  const [showHints, setShowHints] = useState(false); // runs at server render
-
-  const hintToggler = () => {
-    showHints === false ? setShowHints(true) : setShowHints(false);
-    console.log("toggling")
-  };
-
-  useEffect( () => {
-    const theWord = generateRandomWord()
-    console.log(`the word is ${theWord}`)
-    setWord(theWord)
-  }, [])
-
-  const generateRandomWord = ( ) => {
-    const min = 1;
-    const max = 11// wordList.length
-    const rand =  Math.floor(Math.random() * (max - min + 1) + min)
-    const theWord = "hello" // wordList[rand]
-    return theWord
-  }
-
-  // const generateRandomWord = ( ) => {
-  //   const min = 1;
-  //   const max =  wordList.length
-  //   const rand =  Math.floor(Math.random() * (max - min + 1) + min)
-  //   const theWord = wordList[rand]
-  //   return theWord
-  // }
-
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-    console.log("toggling")
-  };
-
-
-  const modalToggler = () => {
-    showModal === true ? setShowModal(false): setShowModal(true)
-  };
-  
   return (
-    <>
-    <Head>
-      <title>WordCheater - The Wordle Solver</title>
-    </Head>
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <StyledApp>
-        <HelpModal showModal={showModal} modalToggler={modalToggler} />
-        <StyledNavbar variant={theme} className="justify-content-between">
-          <Container>
-            <Navbar.Brand onClick={modalToggler}><FaRegQuestionCircle /></Navbar.Brand>
-          </Container>
-          <Container>
-            <Navbar.Brand href="#home">WordCheater</Navbar.Brand>
-          </Container>
-          <Container className="justify-content-end">
-            <ThemeToggleSwitch
-              defaultValue={true}
-              toggleFn={themeToggler}
-              />
-            <HintToggler
-              onClick={hintToggler}
-              >
-              <FaLightbulb />
-            </HintToggler>
-          </Container>
-        </StyledNavbar>
-        <p>wordlist: {wordList}</p>
-        <GameBoard 
-          wordList={wordList}
-          word={word}
-          showHints={showHints}
-          setShowHints={setShowHints}
-          />
-      </StyledApp>
-    </ThemeProvider>
-    </>
+    <div className={styles.container}>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        {/* <!--
+        manifest.json provides metadata used when your web app is installed on a
+        user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+      --> */}
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        {/* <!--
+        Notice the use of %PUBLIC_URL% in the tags above.
+        It will be replaced with the URL of the `public` folder during the build.
+        Only files inside the `public` folder can be referenced from the HTML.
+
+        Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+        work correctly both with client-side routing and a non-root public URL.
+        Learn how to configure a non-root public URL by running `npm run build`.
+      --> */}
+        <title>WordCheater - The Wordle Solver</title>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
+        <meta property="og:title" content="WordCheater - The Wordle Solver" />
+        <meta property="og:image" content="" />
+        <meta
+          property="og:description"
+          content="WordCheater helps you find 5 letter words!"
+        />
+      </Head>
+      <Component />
+    </div>
   );
 }
-
-const StyledApp = styled.div`
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const HintToggler = styled.div`
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const StyledNavbar = styled(Navbar)`
-  border-bottom: 1px solid ${(props) => props.theme.fontColor};
-  left: 50%;
-  transform: translatex(-50%);
-  width:80%;
-  margin-bottom: 2rem;
-`
-
-export default App;
