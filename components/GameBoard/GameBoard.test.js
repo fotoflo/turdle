11a6b057ff8b8f808d11290cced2ab17abb23054
Alex Miller <fotoflo@gmail.com>
2022-03-03 15:21:44 +0800

    reindent gameboard test

--- components/GameBoard/GameBoard.test.js
+++ components/GameBoard/GameBoard.test.js
@@ -1,24 +1,28 @@
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import GameBoard from './GameBoard'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import GameBoard from "./GameBoard";
 
-it('renders', ()=>{
-  const word = "hello"
-  const wordList = ["hello","tests"]
-  const showHints = false
-  const setShowHints = ()=>{}
-  const setWordLength = ()=>{}
+describe("GameBoard Componenent", () => {
+  it("renders", () => {
+    const word = "hello";
+    const wordList = ["hello", "tests"];
+    const showHints = false;
+    const setShowHints = () => {};
+    const setWordLength = () => {};
 
-  render(<GameBoard 
-    word={word}
-    wordList={wordList}
-    showHints={showHints}
-    setShowHints={setShowHints}
-    setWordLength={setWordLength}
-  />)
-  const gameboardContainer = screen.getByTestId("GameboardContainer")
-  expect(gameboardContainer).toBeInTheDocument()
+    render(
+      <GameBoard
+        word={word}
+        wordList={wordList}
+        showHints={showHints}
+        setShowHints={setShowHints}
+        setWordLength={setWordLength}
+      />
+    );
+    const gameboardContainer = screen.getByTestId("GameboardContainer");
+    expect(gameboardContainer).toBeInTheDocument();
 
-  const keyboardContainer = screen.getByTestId("KeyboardContainer")
-  expect(keyboardContainer).toBeInTheDocument()
-})
+    const keyboardContainer = screen.getByTestId("KeyboardContainer");
+    expect(keyboardContainer).toBeInTheDocument();
+  });
+});
