b689f60620d30a74954caaab9236a3e0b381f6d2
Alex Miller <fotoflo@gmail.com>
2022-03-09 02:37:15 +0800

    some env and comments

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+// NEXT_PUBLIC ENV is exposed to the javascript
+// BUILD env only gets updated at build time
+// .env.local overrides .env.development and .env.production
+
 module.exports = {
   reactStrictMode: true,
   MIN_WORD_LENGTH: 3,
@@ -5,8 +9,7 @@ module.exports = {
   DEFAULT_WORD_LENGTH: 3,
   MAX_HINTS: 100,
   DEFAULT_SHOW_HINTS: true,
-  DEFAULT_SHOW_WORD: false,
+  DEFAULT_SHOW_WORD: process.env.NEXT_PUBLIC_BUILD_SHOW_WORD === 'true',
   BASE_URL: process.env.NEXT_PUBLIC_WORDCHEATER_BASE_URL,
-  GA_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ACTIVE,
-  IS_DEV_ENV: process.env.NEXT_PUBLIC_IS_DEV_ENV === 'true'
+  GA_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ACTIVE
 }
