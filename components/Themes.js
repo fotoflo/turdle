b72e7da7e7dc64954ccd9afb9ae4e49010fc4126
Alex Miller <fotoflo@gmail.com>
2022-03-26 12:57:28 +0800

    Update colors, move some style to theme, update max width

--- components/Themes.js
+++ components/Themes.js
@@ -4,29 +4,36 @@ import { createGlobalStyle } from "styled-components";
 const periwinkle = "#CCCCFF"
 
 export const lightTheme = {
-  background: periwinkle,
+  background: "white",
   fontColor: "#000",
   InfoBGColor: 'ivory',
-  0: "white",
-  1: "#65696B", // grey
-  2: "#BEA747", // yellow
-  3: "#599D51" // green
+  0: "white",  // White, Blank
+  1: "#FDA8EF", // not in word, yellow
+  2: "#FFFD0B", // wrong slot
+  3: "#35E52D" // correct slot
 };
 
 export const darkTheme = {
-  background: "#000",
+  background: "black",
   fontColor: "#fff",
   InfoBGColor: '#240026',
-  0: "black",
-  1: "#65696B", // grey
-  2: "#BEA747", // yellow
-  3: "#599D51" // green
+  0: "black",   // black, Blank
+  1: "#FDA8EF", // not in word, yellow
+  2: "#FFFD0B", // wrong slot
+  3: "#35E52D" // correct slot
 };
 
 export const GlobalStyles = createGlobalStyle`
 	body {
+    max-width: 800px;
+    min-width: 300px;
 		background-color: ${ props => props.theme.background };
     color: ${ props => props.theme.fontColor };
     caret-color: transparent;
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      user-select:none;
 	}
 `;
