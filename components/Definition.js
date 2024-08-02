732d607ef4c0d7689a29c2e87ba1f6eed82829cc
Alex Miller <fotoflo@gmail.com>
2024-08-02 17:40:33 +0800

    mmmk kinda wrkng

--- components/Definition.js
+++ components/Definition.js
@@ -5,7 +5,15 @@ import useDefinition from "./hooks/useDefinition";
 const Definition = ({ word }) => {
   const { def } = useDefinition(word);
 
-  return <>{JSON.stringify(def)}</>;
+  return (
+    <div>
+      <li>
+        {def?.[0].meanings.map((meaning) => {
+          return meaning.definitions[0].definition;
+        })}
+      </li>
+    </div>
+  );
 };
 
 export default Definition;
